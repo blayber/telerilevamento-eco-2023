95db3a60edede0348a14c18138fb6dfd4c5e8147
blayber <127427339+blayber@users.noreply.github.com>
2023-03-09 17:40:46 +0100

    Create R_firstcode.R

---
+++ R_firstcode.R
@@ -0,0 +1,5 @@
+# my first code in git hub  
+# let's install the raster package
+instal. packages ("raster")
+
+library                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
